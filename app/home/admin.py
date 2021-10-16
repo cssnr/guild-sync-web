a7b24cf8e2074ce655f9e030ea4fb3c42f9249da
Shane <shane@shanerice.me>
2021-10-16 02:33:24 -0700

    Adding ServerProfile

--- app/home/admin.py
+++ app/home/admin.py
@@ -15,7 +15,7 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 @admin.register(ServerProfile)
 class ServerProfileAdmin(admin.ModelAdmin):
-    list_display = ('server_name', 'server_id', 'guild_name', 'alert_channel', 'is_enabled',)
+    list_display = ('server_id', 'server_name', 'guild_name', 'alert_channel', 'is_enabled',)
     list_filter = ('is_enabled',)
     search_fields = ('server_id', 'server_name', 'guild_name',)
     ordering = ('server_name',)
