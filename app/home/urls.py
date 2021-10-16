b2620316f29377de5f42e6f6ef5f021e00a54c1c
Shane <shane@shanerice.me>
2021-10-15 20:09:18 -0700

    Adding discord server list

--- app/home/urls.py
+++ app/home/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.home_view, name='index'),
     path('news/', views.news_view, name='news'),
     path('profile/', views.profile_view, name='profile'),
-    path('server/<str:serverid>/', views.profile_view, name='server'),
+    path('server/<str:serverid>/', views.server_view, name='server'),
 ]
