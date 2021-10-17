112567f30465bcfcf64a048bda803be806f9ad2e
Shane <shane@shanerice.me>
2021-10-17 16:11:49 -0700

    Settings and oauth update

--- app/discordsync/settings.py
+++ app/discordsync/settings.py
@@ -153,6 +153,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_beat',
     'django_extensions',
     'debug_toolbar',
     'home',
