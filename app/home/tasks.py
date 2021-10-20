0d495c0d05458a2abe14aba0ef277ea179b4e692
Shane <shane@shanerice.me>
2021-10-19 23:50:09 -0700

    Fixed name sync

--- app/home/tasks.py
+++ app/home/tasks.py
@@ -17,60 +17,77 @@ def clear_sessions():
 @task()
 def process_upload(user_pk, data):
     user = CustomUser.objects.get(pk=user_pk)
-    logger.debug(user.server_list)
+    logger.info(user.server_list)
     for g in data['guilds']:
+        logger.info(data['guilds'][g])
         guild = g.split('-')[0]
         realm = g.split('-')[1]
-        logger.debug('guild: %s', guild)
-        logger.debug('realm: %s', realm)
+        logger.info('guild: %s', guild)
+        logger.info('realm: %s', realm)
         server = ServerProfile.objects.get_by_guild(guild, realm)
-        logger.debug(server)
+        logger.info(server)
         if server and server.server_id in user.server_list:
             logger.info('Matching Configuration: %s', server.server_id)
-            logger.debug(server)
+            logger.info(server)
             r = get_guild_users(server.server_id)
             if not r.ok:
                 r.raise_for_status()
             users = r.json()
-            # logger.debug(users)
-            if server.sync_method == 'note':
-                logger.debug('Note Sync.')
-                logger.debug(data['guilds'][g])
-                for user in users:
+            for user in users:
+                if server.sync_method == 'note':
+                    # note sync
+                    logger.info('Note Sync.')
                     discord_user = '{}#{}'.format(
                         user['user']['username'],
                         user['user']['discriminator'],
                     )
                     match = match_note(data['guilds'][g], discord_user)
-                    if match:
-                        logger.debug('Will Process Match Here!')
-                        logger.debug(match)
-                        if server.guild_role and \
-                                server.guild_role not in user['roles']:
-                            logger.debug('Adding %s to role %s',
-                                         user['user']['username'],
-                                         server.guild_role)
-                            r = add_guild_role(server.server_id,
-                                               user['user']['id'],
-                                               server.guild_role)
-                            logger.debug(r.status_code)
-                            logger.debug(r.content.decode('utf-8'))
-                            if not r.ok:
-                                logger.error('Error adding role to user.')
-                            else:
-                                logger.info('Updated user successfully.')
-            elif server.sync_method == 'name':
-                logger.debug('Name Sync.')
-                pass
-            else:
-                logger.warning('Unknown sync_method: %s', server.sync_method)
+                elif server.sync_method == 'name':
+                    # name sync
+                    logger.info('Name Sync.')
+                    discord_user = user['nick'] if user['nick'] else user['user']['username']
+                    match = match_name(data['guilds'][g], discord_user)
+                    pass
+                else:
+                    logger.warning('Unknown sync_method: %s', server.sync_method)
+                    continue
+
+                if match:
+                    logger.info('Will Process Match Here!')
+                    logger.info(match)
+                    if server.guild_role and \
+                            server.guild_role not in user['roles']:
+                        logger.info('Adding %s to role %s',
+                                     user['user']['username'],
+                                     server.guild_role)
+                        r = add_guild_role(server.server_id,
+                                           user['user']['id'],
+                                           server.guild_role)
+                        logger.info(r.status_code)
+                        logger.info(r.content.decode('utf-8'))
+                        if not r.ok:
+                            logger.error('Error adding role to user.')
+                        else:
+                            logger.info('Updated user successfully.')
 
 
-def match_note(guild_data, username):
+def match_note(guild_data, discord_user):
+    logger.info('discord_user: %s', discord_user)
     for user, data in guild_data.items():
-        if data[2] == username:
-            logger.info('Matched User: %s', username)
+        if data[2] == discord_user:
+            logger.info('Matched User: %s', discord_user)
             return user
+    return None
+
+
+def match_name(guild_data, username):
+    logger.info('username: %s', username)
+    for user, data in guild_data.items():
+        if user and username:
+            user_only = user.split('-')[0]
+            if user_only.lower() == username.lower():
+                logger.info('Matched User: %s', username)
+                return user
     return None
 
 
